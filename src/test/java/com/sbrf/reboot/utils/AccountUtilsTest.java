29ae3cd93a73b71edd75dd2622f867a0cf028b70
SobchukP <67798576+SobchukP@users.noreply.github.com>
2021-09-01 23:57:20 +0500

    ДЗ к лекции 6, 7

--- src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
+++ src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 class AccountUtilsTest {
@@ -46,4 +45,26 @@ class AccountUtilsTest {
         Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
         Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
     }
+
+    @Test
+    void sortedByIdDateBalance() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build()); // 1
+            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.ONE).build()); // 0
+            add(Account.builder().id(2L).createDate(LocalDate.now().minusDays(1)).balance(new BigDecimal(2)).build()); // 3
+            add(Account.builder().id(2L).createDate(LocalDate.now().minusDays(2)).balance(new BigDecimal(2)).build()); // 2
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(new BigDecimal(5)).build()); // 5
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(new BigDecimal(3)).build()); // 4
+        }};
+
+        AccountUtils.sortedByIdDateBalance(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getId());
+        Assertions.assertEquals(2L, accounts.get(2).getId());
+        Assertions.assertEquals(LocalDate.now().minusDays(2), accounts.get(2).getCreateDate());
+        Assertions.assertEquals(BigDecimal.TEN, accounts.get(1).getBalance());
+        Assertions.assertEquals(new BigDecimal(3), accounts.get(4).getBalance());
+
+    }
+
 }
