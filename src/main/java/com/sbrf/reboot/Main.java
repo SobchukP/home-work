b759499dbe59e9c0716978e7b3068e61db6ae27c
SobchukP <67798576+SobchukP@users.noreply.github.com>
2021-09-30 20:36:54 +0500

    ДЗ к лекции 15

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -6,6 +6,5 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/com/sbrf/reboot/servlet/MainHttpServlet.java
@@ -0,0 +1,30 @@
+package com.sbrf.reboot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@WebServlet("/home")
+public class MainHttpServlet extends HttpServlet {
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException{
+        HttpSession session = req.getSession();
+        counter.set(counter.get()+1);
+        session.setAttribute("visitCounter", counter);
+        String name = null == req.getParameter("name") ? "Anonymous" : req.getParameter("name");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Привет, " + name + "\nВы " + counter + "-й посетитель");
+        printWriter.close();
+    }
+}
